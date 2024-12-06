7529e5ca19425ec36fe32227e4a58f0308cbb8e7
Natalia Josse <natalia.josse@comarch.pl>
2024-12-06 11:36:42 +0100

    framework refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,26 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import eslintPluginPlaywright from "eslint-plugin-playwright";
 
-export default [{ignores: ["package-lock.json", "playwright-report/**", "test-results/**"]}, {files: ["**/*.ts"]}, {languageOptions: {globals: globals.node}}, pluginJs.configs.recommended, {
-    rules: {"no-console": "error",},
-}, ...tseslint.configs.recommended, {
-    rules: {"@typescript-eslint/explicit-function-return-type": "error",},
-}, eslintPluginPlaywright.configs["flat/recommended"], {
-    rules: {"playwright/no-nested-step": "off",}, settings: {
-        playwright: {
-            globalAliases: {test: ["setup"],},},},
-}, eslintPluginPrettierRecommended,];
+export default [
+  { ignores: ["package-lock.json", "playwright-report/**", "test-results/**"] },
+  { files: ["**/*.ts"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: { "no-console": "error" },
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: { "@typescript-eslint/explicit-function-return-type": "error" },
+  },
+  eslintPluginPlaywright.configs["flat/recommended"],
+  {
+    rules: { "playwright/no-nested-step": "off" },
+    settings: {
+      playwright: {
+        globalAliases: { test: ["setup"] },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
